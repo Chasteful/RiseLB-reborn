f6782dfce7dd3977d4e5cc82872083d7f59fdd13
Chasteful <149867152+Chasteful@users.noreply.github.com>
2025-04-26 22:09:12 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-    base: "",
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: "modern"
-            }
-        }
+  base: './', 
+  build: {
+    outDir: path.resolve('E:/Program Files/Minecraft/Modpacks/.minecraft/versions/LiquidBounce Nextgen/LiquidBounce/themes/1.5.6.042625_RC'),
+    emptyOutDir: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern"
+      }
     }
+  }
 })
