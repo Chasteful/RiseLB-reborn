9e38d5f6cf5f9f85b98effbcfb80bfdbe125429a
Chasteful <149867152+Chasteful@users.noreply.github.com>
2025-04-26 19:37:18 +0800

    Delete useChangeTransition.ts

--- src/integration/useChangeTransition.ts
+++
@@ -1,8 +0,0 @@
-import { tweened } from 'svelte/motion';
-import { cubicOut } from 'svelte/easing';
-
-export function useChangeTransition(from: number, to: number, duration = 450) {
-  const value = tweened(from, { duration, easing: cubicOut });
-  value.set(to);
-  return value;
-}
