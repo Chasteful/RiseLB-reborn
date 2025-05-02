97b95d06767cc34cf993e4f7caf66cc23debba4f
Chasteful <149867152+Chasteful@users.noreply.github.com>
2025-05-02 15:49:34 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import Router, { push } from "svelte-spa-router";
+    import Router, {push} from "svelte-spa-router";
     import ClickGui from "./routes/clickgui/ClickGui.svelte";
     import Hud from "./routes/hud/Hud.svelte";
-    import { getVirtualScreen } from "./integration/rest";
-    import { cleanupListeners, listenAlways } from "./integration/ws";
-    import { onMount } from "svelte";
-    import { insertPersistentData } from "./integration/persistent_storage";
+    import {getVirtualScreen} from "./integration/rest";
+    import {cleanupListeners, listenAlways} from "./integration/ws";
+    import {onMount} from "svelte";
+    import {insertPersistentData} from "./integration/persistent_storage";
     import Inventory from "./routes/inventory/Inventory.svelte";
     import Title from "./routes/menu/title/Title.svelte";
-    import SplashScreen from "./routes/menu/splash/SplashScreen.svelte";
     import Multiplayer from "./routes/menu/multiplayer/Multiplayer.svelte";
     import AltManager from "./routes/menu/altmanager/AltManager.svelte";
     import Singleplayer from "./routes/menu/singleplayer/Singleplayer.svelte";
@@ -21,24 +20,18 @@
         "/hud": Hud,
         "/inventory": Inventory,
         "/title": Title,
-        "/splash": SplashScreen,
         "/multiplayer": Multiplayer,
         "/altmanager": AltManager,
         "/singleplayer": Singleplayer,
         "/proxymanager": ProxyManager,
+
         "/disconnected": Disconnected,
         "/browser": Browser
     };
-   
+
     const url = window.location.href;
     const staticTag = url.split("?")[1];
     const isStatic = staticTag === "static";
-    let showSplash: boolean = false;
-
-    // HACK: Just in case
-    setTimeout(() => {
-        showSplash = false;
-    }, 3 * 1000);
 
     async function changeRoute(name: string) {
         cleanupListeners();
@@ -55,16 +48,7 @@
 
         listenAlways("socketReady", async () => {
             const virtualScreen = await getVirtualScreen();
-            showSplash = virtualScreen.showingSplash;
             await changeRoute(virtualScreen.name || "none");
-        });
-
-        listenAlways("splashOverlay", async (event: any) => {
-            showSplash = event.showingSplash;
-            if (!showSplash) {
-           
-                window.location.replace(window.location.href.split("#").shift()!);
-            }
         });
 
         listenAlways("virtualScreen", async (event: any) => {
@@ -82,15 +66,10 @@
         });
 
         const virtualScreen = await getVirtualScreen();
-        showSplash = virtualScreen.showingSplash;
         await changeRoute(virtualScreen.name || "none");
     });
 </script>
 
 <main>
-    {#if showSplash}
-        <SplashScreen/>
-    {:else}
-        <Router {routes}/>
-    {/if}
+    <Router {routes}/>
 </main>
